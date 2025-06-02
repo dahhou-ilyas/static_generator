d8c9a6c7c90773145a379109c8dd3ef36344b1e2
dahhou.ilyas <dahhou.ilyas@gmail.com>
2025-06-02 15:10:02 +0100

    add process block

---
+++ src/block_process.py
@@ -0,0 +1,36 @@
+from enum import Enum
+import re
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered_list"
+
+
+def block_to_block_type(block:str):
+    if re.match(r"^#{1,6} ", block):
+        return BlockType.HEADING
+
+    if re.match(r"^```(?:.|\n)*?```$", block, re.DOTALL):
+        return BlockType.CODE
+
+    if re.match(r"^(> .*(\n|$))+", block):
+        return BlockType.QUOTE
+
+    if re.match(r"^(- .*(\n|$))+", block):
+        return BlockType.UNORDERED_LIST
+    
+    lines = block.strip().splitlines()
+    is_ordered = True
+    for i, line in enumerate(lines, start=1):
+        if not re.match(rf"^{i}\. .+", line):
+            is_ordered = False
+            break
+    if is_ordered and lines:
+        return BlockType.ORDERED_LIST
+
+    return BlockType.PARAGRAPH
+    
