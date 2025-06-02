b6d5f65adaeaca74089a06254c0e031b404a0efa
dahhou.ilyas <dahhou.ilyas@gmail.com>
2025-06-02 11:04:58 +0100

    add Split Delimiter

---
+++ src/split_dilemeter.py
@@ -0,0 +1,54 @@
+from textnode import TextNode,TextType
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    result = []
+    for node in old_nodes:
+        # On ne modifie que les TextNode de type TEXT
+        if node.text_type != TextType.TEXT:
+            result.append(node)
+            continue
+
+        parts = node.text.split(delimiter)
+        if len(parts) % 2 == 0:
+            raise Exception("Invalid markdown: missing closing delimiter")
+
+        for i, part in enumerate(parts):
+            if i % 2 == 0:
+                if part:
+                    result.append(TextNode(part, TextType.TEXT))
+            else:
+                result.append(TextNode(part, text_type))
+
+    return result
+
+
+""" 
+def extract_inside_outside(text, delimiter):
+    entre = []
+    hors = []
+    i = 0
+    d_len = len(delimiter)
+
+    while i < len(text):
+        if text[i:i+d_len] == delimiter:
+            start = i + d_len
+            end = text.find(delimiter, start)
+            if end != -1:
+                entre.append(text[start:end])
+                i = end + d_len
+            else:
+                # Si on trouve un délimiteur d'ouverture sans fermeture
+                hors.append(text[i:])
+                break
+        else:
+            next_delim = text.find(delimiter, i)
+            if next_delim == -1:
+                hors.append(text[i:])
+                break
+            else:
+                hors.append(text[i:next_delim])
+                i = next_delim
+
+    return entre, hors
+
+ """
--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,5 +1,5 @@
 import unittest
-
+from split_dilemeter import split_nodes_delimiter
 from textnode import TextNode,TextType
 
 class TestTextNode(unittest.TestCase):
@@ -21,6 +21,64 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("This is a text node tialic", TextType.ITALIC)
         node2 = TextNode("This is a text node", TextType.ITALIC)
         self.assertNotEqual(node,node2)
+
+    
+    def test_code_block_split(self):
+        node = TextNode("This is text with a `code block` word", TextType.TEXT)
+        expected = [
+            TextNode("This is text with a ", TextType.TEXT),
+            TextNode("code block", TextType.CODE),
+            TextNode(" word", TextType.TEXT),
+        ]
+        result = split_nodes_delimiter([node], "`", TextType.CODE)
+        self.assertEqual(result, expected)
+
+    def test_bold_split(self):
+        node = TextNode("Hello **world**!", TextType.TEXT)
+        expected = [
+            TextNode("Hello ", TextType.TEXT),
+            TextNode("world", TextType.BOLD),
+            TextNode("!", TextType.TEXT),
+        ]
+        result = split_nodes_delimiter([node], "**", TextType.BOLD)
+        self.assertEqual(result, expected)
+
+    def test_italic_split(self):
+        node = TextNode("This is _italic_ text", TextType.TEXT)
+        expected = [
+            TextNode("This is ", TextType.TEXT),
+            TextNode("italic", TextType.ITALIC),
+            TextNode(" text", TextType.TEXT),
+        ]
+        result = split_nodes_delimiter([node], "_", TextType.ITALIC)
+        self.assertEqual(result, expected)
+
+    def test_multiple_delimiters(self):
+        node = TextNode("A **bold** and another **one**", TextType.TEXT)
+        expected = [
+            TextNode("A ", TextType.TEXT),
+            TextNode("bold", TextType.BOLD),
+            TextNode(" and another ", TextType.TEXT),
+            TextNode("one", TextType.BOLD),
+        ]
+        result = split_nodes_delimiter([node], "**", TextType.BOLD)
+        self.assertEqual(result, expected)
+
+    def test_no_delimiter(self):
+        node = TextNode("Plain text only", TextType.TEXT)
+        result = split_nodes_delimiter([node], "**", TextType.BOLD)
+        self.assertEqual(result, [node])
+
+    def test_non_text_node(self):
+        node = TextNode("bolded", TextType.BOLD)
+        result = split_nodes_delimiter([node], "**", TextType.BOLD)
+        self.assertEqual(result, [node])
+
+    def test_unclosed_delimiter_raises(self):
+        node = TextNode("This is **broken", TextType.TEXT)
+        with self.assertRaises(Exception) as context:
+            split_nodes_delimiter([node], "**", TextType.BOLD)
+        self.assertIn("Invalid markdown", str(context.exception))
 
 if __name__ == "__main__":
     unittest.main()
