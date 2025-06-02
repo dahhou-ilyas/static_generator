26a2921d1e73b85e83b999d6d6d2bc5c48078056
dahhou.ilyas <dahhou.ilyas@gmail.com>
2025-06-02 16:06:52 +0100

    discvoer some errorts

--- src/main.py
+++ src/main.py
@@ -1,7 +1,19 @@
 from textnode import TextNode,TextType
+from split_dilemeter import markdown_to_html_node
 def main():
-    textnode=TextNode("This is some anchor text",TextType.LINK,"https://www.boot.dev")
-    print(textnode)
+    md = """
+This is **bolded** paragraph
+text in a p
+tag here
+
+This is another paragraph with _italic_ text and `code` here
+
+"""
+    node = markdown_to_html_node(md)
+    html = node.to_html()
+
+    print(html)
+
 
 if __name__ == "__main__":
     main()
